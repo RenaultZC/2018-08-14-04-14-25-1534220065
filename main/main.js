874a73583d1f1f8cab9837b1e30fdf0c650a49f9
zhangchao <1073294992@qq.com>
2019-05-21 20:58:09 +0800

    result

--- main/main.js
+++ main/main.js
@@ -1,4 +1,27 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+  num += ''
+  let arr = [
+    '._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.',
+    '|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|',
+    '|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|'
+  ]
+  let result = [
+    [],
+    [],
+    []
+  ]
+  for (let i = 0; i < 3; i ++) {
+    arr[i] = arr[i].split('   ')
+  }
+  for (let i = 0, len = num.length; i < len ; i ++) {
+    result[0].push(arr[0][num[i]])
+    result[1].push(arr[1][num[i]])
+    result[2].push(arr[2][num[i]])
+  }
+  result[0] = result[0].join(' ')
+  result[1] = result[1].join(' ')
+  result[2] = result[2].join(' ')
+  result = result.join('\n')
+  result += '\n'
+  return result
 };
